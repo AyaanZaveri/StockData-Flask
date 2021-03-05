bc14da8c569b26858de7dfd284b53915a2a37d31
FatSandwich <ayaanzaveri08@gmail.com>
2021-03-05 10:47:40 -0500

    changes

--- app.py
+++ app.py
@@ -15,31 +15,38 @@ def process():
   name = request.form.get('name','')
   session['name'] = name.title()
 
-  if name == '':
-      name = 'bitcoin'.title()
+  url = "https://ca.finance.yahoo.com/quote/" + name
 
-  url = "https://coinmarketcap.com/currencies/" + name
+  url2 = 'https://ca.finance.yahoo.com/quote/'+ name +'/key-statistics'
 
-  print(url)
+  r = requests.get(url)
+  s = requests.get(url2)
 
-  r = requests.get(url)  
+  soup = BeautifulSoup(r.text, 'html.parser')
+  soup2 = BeautifulSoup(s.text, 'html.parser')
 
-  soup = BeautifulSoup(r.text, 'html.parser')  
 
-  cryptoname = soup.find('p', {
-      'class': 'sc-AxhUy ghbWav converter-item-name'
-  }).text
-  
+  lossgain = soup.find('div', {'class': 'D(ib) Mend(20px)' }).find_all('span')[1].text
 
-  cryptoval = soup.find('div', {'class':   'priceValue___11gHJ'}).text  
+  print(lossgain)
 
-  cryptostrip = cryptoval[1:]  
+  price = soup.find('span', {'class': 'Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)' }).text
 
-  print(cryptoname + ': ' + cryptostrip + ' USD')
+  print(price)
 
-  name.title()
+  marketcap = soup.find('div', {'class': 'D(ib) W(1/2) Bxz(bb) Pstart(12px) Va(t) ie-7_D(i) ie-7_Pos(a) smartphone_D(b) smartphone_W(100%) smartphone_Pstart(0px) smartphone_BdB smartphone_Bdc($seperatorColor)' }).find_all('span')[1].text
 
-  return render_template('index.html', data=cryptostrip, name=name)
+  print(marketcap)
+
+  volume = soup.find('div', {'class': 'D(ib) W(1/2) Bxz(bb) Pend(12px) Va(t) ie-7_D(i) smartphone_D(b) smartphone_W(100%) smartphone_Pend(0px) smartphone_BdY smartphone_Bdc($seperatorColor)' }).find_all('span')[11].text
+
+  print(volume)
+
+  enterpriseval = soup2.find('tr', {'class': 'Bxz(bb) H(36px) BdB Bdbc($seperatorColor) fi-row Bgc($hoverBgColor):h'}).find_all('td')[1].text
+
+  print(enterpriseval)
+
+  return render_template('index.html', price=price, name=name)
   
 if __name__ == '__main__':
   app.run(host="0.0.0.0", threaded=True, port=5000)
